2163ee7bb102bbc9a0f70b147913b275f28b3e58
Lazir Pascual <lazirpascual@gmail.com>
2021-07-14 20:39:48 -0400

    set up cartitems collection

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan");
 require("dotenv").config();
-const Product = require("./models/product");
+const { Product, CartItem } = require("./models/model");
 
 app.use(cors());
 app.use(express.json());
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+
+app.use(
+  morgan(":method :url :status :res[content-length] :response-time ms :body")
+);
 
 app.get("/api/products", (request, response) => {
   Product.find({}).then((products) => {
     response.json(products);
   });
 });
+
+app.get("/api/cartitems", (request, response) => {
+  CartItem.find({}).then((products) => {
+    response.json(products);
+  });
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- models/model.js
+++ models/model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI_SHOP;
 
 console.log("connecting to", url);
 
@@ -28,6 +28,16 @@ const productSchema = new mongoose.Schema({
   productPreview: String,
 });
 
+const cartitemSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  quantity: Number,
+  category: String,
+  imgPath: String,
+  imgPreview: String,
+  productPreview: String,
+});
+
 productSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
@@ -36,4 +46,15 @@ productSchema.set("toJSON", {
   },
 });
 
-module.exports = mongoose.model("Product", productSchema);
+cartitemSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+const CartItem = mongoose.model("Cartitem", cartitemSchema);
+
+module.exports = { Product, CartItem };
