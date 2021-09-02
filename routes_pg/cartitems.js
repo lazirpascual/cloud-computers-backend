7ba1ab1620fc660d1dfdb4ce517d786465e52d85
Lazir Pascual <lazirpascual@gmail.com>
2021-09-02 12:48:30 -0400

    create cartitems route for postgres

--- routes_pg/cartitems.js
+++ routes_pg/cartitems.js
@@ -1,3 +1,83 @@
 const cartItemsRouter = require("express").Router();
+const pool = require("../db");
 
-const pool = require("../utils/db");
+// get all cartitems
+cartItemsRouter.get("/", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM cartitems");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// get a specific cartitem
+cartItemsRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM cartitems WHERE id = $1", [
+      id,
+    ]);
+    res.status(200).json(todo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// create a cartitem
+cartItemsRouter.post("/", async (req, res) => {
+  try {
+    const {
+      name,
+      price,
+      quantity,
+      category,
+      imgPath,
+      imgPreview,
+      productPreview,
+    } = req.body;
+    const newCartItem = await pool.query(
+      "INSERT INTO cartitems (name, price, quantity, category, imgPath, imgPreview, productPreview) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [name, price, quantity, category, imgPath, imgPreview, productPreview]
+    );
+    res.status(200).json(newCartItem.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// delete a cartitem
+cartItemsRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query("DELETE FROM cartitems WHERE id = $1", [id]);
+    res.status(200).json("Item was deleted!");
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// update a cartitem
+cartItemsRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      name,
+      price,
+      quantity,
+      category,
+      imgPath,
+      imgPreview,
+      productPreview,
+    } = req.body;
+    const updatedCartItem = await pool.query(
+      "UPDATE cartitems SET name = ($1), price = ($2), quantity = ($3), category = ($4), imgPath = ($5), imgPreview = ($6), productPreview = ($7) WHERE id = $8 RETURNING *",
+      [name, price, quantity, category, imgPath, imgPreview, productPreview, id]
+    );
+    res.status(200).json(updatedCartItem.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+module.exports = cartItemsRouter;
