3acf358c73e891e2c2d30f278b5d8256819ff67c
Lazir Pascual <lazirpascual@gmail.com>
2021-09-01 20:22:44 -0400

    create cartitem model for postgres

---
+++ models_pg/cartitem.sql
@@ -0,0 +1,23 @@
+/* create cartitem table*/
+CREATE TABLE cartitems(
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(100),
+    price DOUBLE PRECISION NOT NULL,
+    quantity INT NOT NULL,
+    category VARCHAR(30) NOT NULL,
+    imgPath TEXT NOT NULL,
+    imgPreview TEXT NOT NULL,
+    productPreview TEXT NOT NULL
+);
+
+/* add data to table */
+INSERT INTO cartitems (name, price, quantity, category, imgPath, imgPreview, productPreview)
+VALUES (
+    'Fractal Design Meshify C', 
+    95.99, 
+    1, 
+    'Case', 
+    'https://i.imgur.com/b3ieUZw.jpg', 
+    'https://i.imgur.com/iOrrWY6.jpg', 
+    'https://i.imgur.com/x4CmFCt.png'
+);
